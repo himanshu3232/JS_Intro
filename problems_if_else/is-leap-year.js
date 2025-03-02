db6ffb0dfb6a71bf5b3a3fc479ffa41bf99d29a3
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-03-02 18:24:26 +0530

    [Himanshu Ranjan] Add. merge branches

---
+++ problems_if_else/is-leap-year.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return `${year} is a Leap Year.`;
+    } else {
+        return `${year} is not a Leap Year.`;
+    }
+}
+let year = prompt("Enter a 4-digit year:");
+alert(isLeapYear(parseInt(year, 10)));
